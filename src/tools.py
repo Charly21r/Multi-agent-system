1eb53aec695c1d48c5118379948133c8a2e82778
Charly21r <crrubio02@gmail.com>
2025-04-28 00:24:54 +0200

    Fix error in the tool's schema

--- src/tools.py
+++ src/tools.py
@@ -1,7 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Any
-from agents import FunctionTool, RunContextWrapper
-from datetime import date
+from agents import FunctionTool, function_tool
 
 class WeatherToolArgs(BaseModel):
     """
@@ -11,31 +10,22 @@ class WeatherToolArgs(BaseModel):
     location (str): Location of the place to check the weather
     weather_date (date): Date to check the weather
     """
-    location: str
-    weather_date: date
+    location: str = Field(..., description="Location")
+    weather_date: str = Field(..., description="Date")
 
 
-def check_weather(args: WeatherToolArgs) -> str:
+@function_tool
+def WeatherTool(location: WeatherToolArgs) -> str:
     """
     Calls a weather API to check the weather
     
     Parameters:
     args (WeatherToolArgs): An instance of WeatherToolArgs.
-
     Returns:
     str: The weather on the given location and date
     """
     # parsed = WeatherToolArgs.model_validate_json(args)
     
     #TODO: Implement weather api calling
-    return f"""The weather will be sunny and amazing, with a little rain on Turedat"""
-    # return f"""The weather will be amazing in {parsed.location} on the {parsed.weather_date}"""
+    return f"""The weather will be sunny and amazing, with a little rain on Tuesday"""
 
-
-
-WeatherTool = FunctionTool(
-    name="check_weather",
-    description="Checks weather for a given location and date",
-    params_json_schema=WeatherToolArgs.model_json_schema(),
-    on_invoke_tool=check_weather,
-)
